44ab2321277bd5f98ac22ab5d60113e03839e42f
Matthew Novelli <mattnovelli@gmail.com>
2022-11-29 18:59:40 -0600

    code documentation and cleanup

--- newsletter/populate_newsletter.js
+++ newsletter/populate_newsletter.js
@@ -1,15 +1,17 @@
 
-// const body = document.querySelector('#recently-played-section');
-
+// Filepath for json events
 const eventJSON = "./example-events.json"
 
+/**
+ * Appends a new event to <body>
+ * @param event - an event, in json format
+ */
 function newEvent(event) {
     const eventcontainer = document.createElement('div')
     eventcontainer.id = "eventcontainer";
 
     const calendarcontainer = document.createElement('div')
     calendarcontainer.id = "calendarcontainer";
-
 
     const date_array = event["date"].split(" ");
 
@@ -30,15 +32,11 @@ function newEvent(event) {
     const h3 = document.createElement('h3')
     h3.textContent = event["location"];
 
-    // const h3 = document.createElement('h3')
-    // h3.textContent = ;
-
     const link = document.createElement('a')
     link.id = "learnmore"
     link.href = event["url"];
     link.target = "_blank"
     link.textContent = "Learn More >>";
-
 
     eventcontent.append(h2, h3,event["description"], link)
     calendarcontainer.append(calendarmonth, calendarday)
@@ -47,18 +45,23 @@ function newEvent(event) {
     document.body.appendChild(eventcontainer)
 }
 
+/**
+ * Iterates through all events in a JSON array, then adds footer
+ * @param events - JSON array of all events
+ */
 function populate(events) {   
     for(let i = events.length - 1; i != 0; i--) {
         console.log(events[i])
         newEvent(events[i])
     }
-
     const footer = document.createElement('footer')
     footer.textContent = "❉ this email was sent from touchGrass ❉ ";
     document.body.appendChild(footer)
-
 }
 
+/**
+ * Driver code to fetch JSON.
+ */
 fetch(eventJSON)
   .then((response) => response.json())
   .then((data) => populate(data));
